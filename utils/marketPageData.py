ef6fee85d54e486a0aa567074e4ec0029042d759
ZoranUTF8 <janjiczoran23@gmail.com>
2022-11-21 20:54:04 +0100

    Add items list

---
+++ utils/marketPageData.py
@@ -0,0 +1,5 @@
+items = [
+    {"id": 1, "name": "IPhone", "barcode": "4124124124124", "price": 4212},
+    {"id": 1, "name": "Laptop", "barcode": "6546235932595", "price": 5421},
+    {"id": 1, "name": "Monitor", "barcode": "543526765876", "price": 2131}
+]
