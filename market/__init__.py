2367d15260b54a37728a45107624f5a09ff34f3b
ZoranUTF8 <janjiczoran23@gmail.com>
2022-11-23 21:48:23 +0100

    Add flash messages to backend and frontend

--- market/__init__.py
+++ market/__init__.py
@@ -5,11 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.app_context().push()
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///marketDatabase.db"
 app.config["SECRET_KEY"] = "e4194bfabcb346ff60e510e4"
 db = SQLAlchemy(app)
 
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 from market.routes import routes
--- readMe.txt
+++ readMe.txt
@@ -4,3 +4,5 @@ Thats why we use packages to put the whole aplication inside
 __init__ - executes the import codes first for our app in each directory present
 
 forms need secret key
+
+what is with ?
